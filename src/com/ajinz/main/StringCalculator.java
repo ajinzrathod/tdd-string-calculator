fd9dd4d972b3d998304bd4f8a31a2c71e808e1e3
ajinkyarathod <83880373+ajinkyarathod@users.noreply.github.com>
2021-08-12 09:25:23 +0530

    Refactored, Reduced Messy Lines and made clean code using regex

--- src/com/ajinz/main/StringCalculator.java
+++ src/com/ajinz/main/StringCalculator.java
@@ -1,14 +1,13 @@
 package com.ajinz.main;
+
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
     public int Add(String numbers){
-        String delimiter, negative_numbers_str = "";
-        int index2, index1, multiple = 0;
+        String delimiter, negative_numbers_str = "", split_numbers[];
         List<Integer> negative_numbers = new ArrayList<>();
 
         // Removing all Spaces
@@ -17,51 +16,46 @@ public class StringCalculator {
         // for an empty string
         if (numbers == "") return 0;
 
-        // first line delimiter
-        // find if delimiter exists inside []
-        index1 = numbers.indexOf("//[");
-
-        if (index1 == -1) {
-            index1 = numbers.indexOf("//");
-            index2 = numbers.indexOf("\\n");
-        }
-        else{
-            index1 += 1;
-            index2 = numbers.indexOf("]\\n");
-            multiple = 1;
-        }
-
-        // to store unique delimiters
-        Set<Character> multiple_delimiters = new HashSet<>();
+        int index1 = numbers.indexOf("//");
+        int index2 = numbers.indexOf("\\n");
 
         // checking if delimiter exists in first line
         if (index1 != -1) {
             delimiter = numbers.substring(index1 + 2, index2);
-
-            // Removing braces like this: []
-            delimiter = delimiter.replaceAll("[\\[\\]]", "");
-            for(int i = 0; i < delimiter.length(); i++)
-                multiple_delimiters.add(delimiter.charAt(i));
-
-            // because extra ] is added
-            if (multiple == 1) index2++;
-
+            System.out.println(delimiter);
             numbers = numbers.substring(index2 + 2);
-            numbers = numbers.replaceAll(Pattern.quote(delimiter), ",");
+            System.out.println(numbers);
 
-            // replacing each delimiter with comma
-            for(Character m:multiple_delimiters){
-                String ms = String.valueOf(m);
-                numbers = numbers.replaceAll(Pattern.quote(ms), " ");
+            if (delimiter.endsWith("]") && delimiter.indexOf("[") == 0) {
+                // . anything
+                // * zero or more
+                // ? non capturing group
+                Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+                Matcher matcher = pattern.matcher(delimiter);
+
+                int groupCount = matcher.groupCount();
+                System.out.println("GroupCount" + groupCount);
+
+                while (matcher.find()) {
+                    for (int i = 0; i <= groupCount; i++) {
+                        String m = matcher.group(i);
+                        System.out.println("Group " + i + ": " + m);
+                        numbers = numbers.replaceAll(Pattern.quote(m), ",");
+                    }
+                }
+                delimiter = ",";
             }
-            // Replacing multiple spaces with comma
-            numbers = numbers.replaceAll(" +", ",");
-            delimiter = ",";
-        }
-        else delimiter = ",|\n";
 
-        // Splitting numbers with delimiter
-        String[] split_numbers = numbers.split(delimiter);
+            // Splitting numbers with delimiter
+            split_numbers = numbers.split(Pattern.quote(delimiter));
+        }
+        else {
+            System.out.println(numbers);
+            delimiter = ",|\n";
+
+            // Splitting numbers with delimiter
+            split_numbers = numbers.split(delimiter);
+        }
 
         // Counting Total of Numbers
         int total = 0;
